e549239070b584093af5ed135d81f658ece9a084
MC-Creator-Jerry <jerryjwx@163.com>
2026-09-17 18:58:14 +0800

    Update common.js (2026-09-17 18:58)

--- common.js
+++ common.js
@@ -1025,7 +1025,7 @@ window.XLTopics = (function () {
     if (window.XLEdit) return;
     // 注意：脚本引用带 ?v= 版本号，src 不再以 "common.js" 结尾，必须用 *=
     var s = document.createElement('script');
-    s.src = scriptPrefix() + 'editbar.js?v=20260914a';
+    s.src = scriptPrefix() + 'editbar.js?v=20260917c';
     s.async = true;
     document.head.appendChild(s);
   }
